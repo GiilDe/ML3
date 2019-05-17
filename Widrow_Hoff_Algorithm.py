db1c8329826f9e166326569ef6f421553f369965
GiilDe <38991777+gilad212387@users.noreply.github.com>
2019-05-17 19:24:15 +0300

    radio bla bla

---
+++ Widrow_Hoff_Algorithm.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.datasets import load_digits
+
+
+def make_y(c):
+    def y(x_index, data):
+        return 1 if data.target[x_index] == c else -1
+    return y
+
+
+class OvA:
+    def __init__(self):
+        self.W = None
+
+    def fit(self, data, times=1000, learning_rate=0.1):
+        self.W = None
+
+        for c in data.target_names:
+            y = make_y(c)
+            model_c = WHALG(data, y, times, learning_rate)
+            if self.W is None:
+                self.W = np.array(model_c).reshape(-1, 1)
+            else:
+                self.W = np.append(self.W, model_c, axis=1)
+
+    def predict(self, X):
+        Y = X@self.W
+        res = []
+        for row in Y:
+            r = max(row, key=lambda x: x-1 if x >= 0 else x+1)
+            res.append(1 if r > 0 else -1)
+        return res
+
+
+def WHALG(dataset, y_func, times, learning_rate):
+    N = dataset.data.shape[0]
+    M = dataset.data.shape[1]
+    w = np.zeros(M)
+
+    for t in range(times):
+        ind = np.random.randint(0, N)
+        x = dataset.data[ind]
+        y_hat = x.transpose()@w
+        y = y_func(ind, dataset)
+        for w_j, x_j in zip(w, x):
+            w_j += learning_rate*(y - y_hat)*x_j
+
+    return w
+
+
+if __name__ == '__main__':
+    digits = load_digits()
+    classifier = OvA()
+    classifier.fit(digits)
+
